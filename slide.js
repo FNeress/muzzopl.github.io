e179b8a90e5701f0218bce4936935c8865e5c4f8
MuzzoPL <167222651+MuzzoPL@users.noreply.github.com>
2024-06-21 11:35:42 -0300

    carrosel

--- slide.js
+++ slide.js
@@ -1,4 +1,3 @@
-
 var radio = document.querySelector('.manual-btn')
 var cont= 1
 
@@ -6,7 +5,7 @@ document.getElementById('radio1').checked = true
 
 setInterval(() => {
     proximaImg()
-}, 6000)
+}, 4000)
 
 
 function proximaImg(){
@@ -17,23 +16,6 @@ function proximaImg(){
     }
     document.getElementById('radio'+cont).checked = true
 }
-// Adiciona funcionalidade às setas
-document.querySelector('.left-arrow').addEventListener('click', () => {
-    cont--;
-    if (cont < 1) {
-        cont = 3;
-    }
-    document.getElementById('radio' + cont).checked = true;
-});
-
-document.querySelector('.right-arrow').addEventListener('click', () => {
-    cont++;
-    if (cont > 3) {
-        cont = 1;
-    }
-    document.getElementById('radio' + cont).checked = true;
-});
-
 window.addEventListener('scroll', function() {
     var elementos = document.querySelectorAll('.texto-animado');
     
@@ -49,52 +31,6 @@ window.addEventListener('scroll', function() {
     });
 });''
 
-const track = document.querySelector('.carousel-track');
-const setaEsquerda = document.querySelector('.seta-esquerda');
-const setaDireita = document.querySelector('.seta-direita');
-const items = document.querySelectorAll('.carousel-item');
-const itemWidth = items[0].getBoundingClientRect().width + 10; // 10px for margin-right
-let currentIndex = 0;
-
-function updateTrackPosition() {
-    track.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-}
-
-setaEsquerda.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = items.length - 3; // Vai para o último conjunto de 3 itens
-    }
-    updateTrackPosition();
-    resetAutoSlide();
-});
-
-setaDireita.addEventListener('click', () => {
-    if (currentIndex < items.length - 3) {
-        currentIndex++;
-    } else {
-        currentIndex = 0; // Volta para o início
-    }
-    updateTrackPosition();
-    resetAutoSlide();
-});
-
-function autoSlide() {
-    if (currentIndex < items.length - 3) {
-        currentIndex++;
-    } else {
-        currentIndex = 0;
-    }
-    updateTrackPosition();
-}
-
-let autoSlideInterval = setInterval(autoSlide, 3000);
-
-function resetAutoSlide() {
-    clearInterval(autoSlideInterval);
-    autoSlideInterval = setInterval(autoSlide, 3000);
-}
 
 const carousel = document.querySelector(".carousel");
 const arrowBtns = document.querySelectorAll(".wrapper i");
@@ -155,7 +91,7 @@ const infiniteScroll = () => {
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("no-transition");
     }
-
+     
     clearTimeout(timeoutId);
     if (!wrapper.matches(":hover")) autoPlay();
 };
