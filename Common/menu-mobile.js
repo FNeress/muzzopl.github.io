40531bb16d5a0a7fdd6bdd9292ecc937e828256a
MuzzoPL <167222651+MuzzoPL@users.noreply.github.com>
2024-09-19 23:25:27 -0300

    primeira versão

---
+++ Common/menu-mobile.js
@@ -0,0 +1,31 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+window.addEventListener('scroll', function() {
+    var elementos = document.querySelectorAll('.texto-animado');
+    
+    elementos.forEach(function(elemento) {
+        var posicaoElemento = elemento.getBoundingClientRect().top;
+        var posicaoTela = window.innerHeight;
+
+        if (posicaoElemento < posicaoTela) {
+            elemento.classList.add('aparecendo');
+        } else {
+            elemento.classList.remove('aparecendo');
+        }
+    });
+});''
+
